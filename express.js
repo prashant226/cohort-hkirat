d94b0bc7aa83088fbb2f80d667b701e5ed2a9bc5
prashant226 <prashantnaraniya2003@gmail.com>
2024-04-07 13:09:19 +0530

    commited

--- express.js
+++ express.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
 
 
-app.post('/conversation', (req, res) => {
-    console.log(req.headers["authorization"])
-    res.send({
-        msg: "2+2==4"
-    })
+app.use(bodyParser.json());
+
+
+
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+
+    res.send('hello world')
 })
 
 
