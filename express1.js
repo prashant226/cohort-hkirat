15dd75fefb85d56540dfa2dfb39c34a228f1d2ba
prashant226 <prashantnaraniya2003@gmail.com>
2024-04-07 15:00:43 +0530

    commmited

--- express1.js
+++ express1.js
@@ -1,9 +1,41 @@
 const express = require('express')
 const app = express();
+const port = 3000;
 
+
+const users = [{
+    name: "john",
+    kidneys: [{
+        healthy: false
+    }]
+
+}];
+// function sum(n) {
+//     let ans = 0;
+//     for (let i = 0; i <= n; i++) {
+//         ans = ans + i;
+//     }
+//     return ans;
+// }
 app.get('/', function(req, res) {
-    res.send("hi there");
+    // const n = req.query.n;
+    // const ans = sum(n)
+    // res.send("hi there ur ans is " + ans);
+    const johnKidneys = users[0].kidneys;
+    const numberOfKidneys = johnKidneys.length;
+    let numberOfhealthyKidneys = 0;
+    for (let i = 0; i < johnKidneys.length; i++) {
+        if (johnKidneys[i].healthy)
+            numberOfhealthyKidneys = numberOfhealthyKidneys + 1;
 
+    }
+
+    const numberOfUnhealthyKidneys = numberOfKidneys - numberOfhealthyKidneys;
+    res.json({
+        numberOfKidneys,
+        numberOfhealthyKidneys,
+        numberOfUnhealthyKidneys
+    })
 })
 
 
