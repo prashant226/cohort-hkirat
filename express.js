cc5a47f10f214f3f4c5c8b823302a006eb116889
prashant226 <prashantnaraniya2003@gmail.com>
2024-04-07 12:38:37 +0530

    commited

--- express.js
+++ express.js
@@ -3,6 +3,12 @@ const app = express()
 const port = 3000
 
 
-app.get('/', (req, res) => {
-    res.send('hello world')
+app.post('/conversation', (req, res) => {
+    console.log(req.headers["authorization"])
+    res.send({
+        msg: "2+2==4"
+    })
 })
+
+
+app.listen(port)
